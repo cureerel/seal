8a4dfb40db0eee872ab45b7b9e2ae3438d6a360e
CURE <nikburry@gmail.com>
2026-06-16 17:47:24 +0530

    chore: project setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# Seal
+
+My personal tokenizer built from scratch. This tool will breaks down raw text into tokens for natural language processing.
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+text = "A seal is a semiaquatic marine mammal belonging to a diverse group called pinnipeds."
+
+tokens = text.split()
+
+print(tokens,)
+print(type(tokens))
